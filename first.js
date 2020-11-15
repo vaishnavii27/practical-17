de577cb239de6499f9dd6c3975af5be03a025ff0
vaishnavi <vaishnavishivhare306@gmail.com>
2020-11-15 22:50:26 +0530

    committed

--- first.js
+++ first.js
@@ -32,17 +32,17 @@ function setGreeting(){
     let today=new Date();
     let hour=today.getHours();
     if(hour<12){
-        document.body.style.backgroundImage='url("images/morning.jpg")';
+        document.body.style.backgroundImage='url("images/morning1.jpg")';
         greeting1.innerHTML='Good Morning !!!';
         document.body.style.color="black";
     }
     else if(hour<18){
-        document.body.style.backgroundImage='url("images/afternoon.jpg")';
+        document.body.style.backgroundImage='url("images/afternoon1.jpg")';
         greeting1.innerHTML='Good Afternoon !!!';
         document.body.style.color="Navy";
     }
     else{
-        document.body.style.backgroundImage='url("images/evening.jpg")';
+        document.body.style.backgroundImage='url("images/evening1.jpg")';
         greeting1.innerHTML='Good Evening !!!';
         document.body.style.color="white";
     }
